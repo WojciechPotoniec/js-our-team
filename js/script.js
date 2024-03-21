296d4cfe8b26641445577294c401257f8c0717a3
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-03-21 16:33:43 +0100

    created an array of teamuser

---
+++ js/script.js
@@ -0,0 +1,72 @@
+/*
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+
+BONUS 2:
+Organizzare i singoli membri in card/schede
+
+BONUS 3:
+Permettere l'aggiunta di un nuovo membro del team
+
+Consigli del giorno:
+Ragioniamo come sempre a step.
+Prima la logica in italiano e poi traduciamo in codice.
+E ricordiamoci che console.log() è nostro amico!
+
+
+DATI PER ESERCIZIO
+Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
+Angela Caroll	        Chief Editor	                angela-caroll-chief-editor.jpg
+Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
+Angela Lopez	        Social Media Manager	angela-lopez-social-media-manager.jpg
+Scott Estrada	        Developer	                 scott-estrada-developer.jpg
+Barbara Ramos	Graphic Designer	         barbara-ramos-graphic-designer.jpg
+*/
+
+//Creo un array di object 
+
+const team = [
+  user = {
+    name: 'Wayne',
+    surname: 'Barnett',
+    role: 'Founder & CEO'
+  },
+  user1 = {
+    name: 'Angela',
+    surname: 'Caroll',
+    role: 'Chief Editor'
+  },
+  user2 = {
+    name: 'Walter',
+    surname: 'Gordon',
+    role: 'Office Manager'
+  },
+  user3 = {
+    name: 'Angela',
+    surname: 'Lopez',
+    role: 'Social Media Manager'
+  },
+  user4 = {
+    name: 'Scott',
+    surname: 'Estrada',
+    role: 'Developer'
+  },
+  user5 = {
+    name: 'Barbara',
+    surname: 'Ramos',
+    role: 'Graphic Designer'
+  }
+];
+console.log(team);
